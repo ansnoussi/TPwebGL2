03ec1a14cbb9fd14b32bdb615edeb96c1cc2cad2
ansnoussi <anis0snoussi@gmail.com>
2019-04-13 14:33:56 +0100

    fixed login

--- login_submit.php
+++ login_submit.php
@@ -6,19 +6,31 @@ if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
     $email = $_POST['email'];
     $userPassword = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
     require_once('db_connection.php');
-    $stmt = $conn->prepare("SELECT * FROM users WHERE `email`=:email AND `password`=:password LIMIT 1");
+    $stmt = $conn->prepare("SELECT * FROM users WHERE `email`=:email LIMIT 1");
     try {
-        $stmt->execute([':email' => $email, ':password' => $userPassword]);
+        $stmt->execute([':email' => $email ]);
     } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo( "Connection failed: " . $e->getMessage() );
         header('Location: login.php');
     }
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    if (count($stmt->fetchAll()) > 0) {
-        header('Location: index.php');
-        exit(200);
+    
+    foreach($stmt->fetchAll() as $item){
+    if(password_verify($_POST['password'], $item['password'])){
+    session_start();
+    $_SESSION['user'] = [
+        'name' => $item['name'],
+        'email' => $item['email'],
+        'connection_time' => time()
+        ];
+    header('Location: index.php');
+    exit(200);
+    $conn = null; 
+    }else{
+    die("password_verify didn't work");
+    header('Location: login.php');
     }
-    $conn = null;
+    }
 }
-header('Location: login.php');
+
